6f8d548b21f9a52132d5bf1d278db1017986eaea
Enrique Goberna <egobernagarcia@gmail.com>
2025-09-16 08:43:52 +0200

    Adding first post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,10 @@
 var store = [{
+        "title": "Order Tracking — First Steps into My Portfolio",
+        "excerpt":"The first concrete project in my portfolio is an Order Tracking service. At its core, it’s simple: an API to create orders, update their status, and check where they are in the delivery process. But even a small project like this can already show the kind of engineering mindset I...","categories": ["portfolio","projects"],
+        "tags": ["order-tracking","java","spring-boot","postgres","portfolio"],
+        "url": "/blog/order-tracking-first-steps/",
+        "teaser": null
+      },{
         "title": "Welcome to the Blog: Building My Portfolio",
         "excerpt":"As a software engineer with more than nine years of experience — I’ve spent most of my career building scalable, high-volume data systems for e-commerce. Over time, I’ve realized something very clear: no matter how much you grow in the industry, having a personal portfolio is no longer optional. It’s...","categories": ["portfolio","personal","career"],
         "tags": ["software-engineering","portfolio","career-growth","blogging"],
