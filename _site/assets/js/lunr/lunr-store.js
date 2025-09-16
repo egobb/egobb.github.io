4eb92ede52737e66c538cc2704c4a23dd9f3864a
Enrique Goberna <egobernagarcia@gmail.com>
2025-09-16 07:28:54 +0200

    Fixing blog style

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Hello, Dark Minimalist",
+        "excerpt":"This is the first post of the new dark minimalist blog. Built on Jekyll + Minimal Mistakes (remote theme). Styled with a custom dark skin and light typography. Ships with pagination, SEO, sitemap and RSS by default. Next steps: Tweak the accent color. Publish short notes about your portfolio work-in-progress....","categories": ["notes"],
+        "tags": ["setup","jekyll","github-pages"],
+        "url": "/blog/hello-dark-minimalist/",
+        "teaser": null
+      }]
